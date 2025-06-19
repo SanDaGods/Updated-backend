8b8cd12162d510dea83fd5a5b55cbdb4657f7b42
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-19 10:47:03 +0800

    Update constants.js

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 module.exports = {
   JWT_SECRET:
     process.env.JWT_SECRET ||
-    "21b4f89a96ecde2d4d88285d15da09017466707bfd345b7d02bd587fb6981e6a",
+    "3T33@APPR0@GR!M",
   PORT: process.env.PORT || 3000,
 };
