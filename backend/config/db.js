3c8d4c07ee6cc137dd9b854ceb225bd93402f2d8
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-19 11:12:34 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,11 @@
 const mongoose = require("mongoose");
-const conn = mongoose.connection;
-const { GridFSBucket, ObjectId } = require("mongodb");
-const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://rtuiflde:rtu_ifldeDB01@cluster0.7ozrymz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://rtuiflde:rtu_ifldeDB01@cluster0.7ozrymz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
-    console.log("MongoDB connected sucessfully");
+    console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error!", err);
     process.exit(1);
